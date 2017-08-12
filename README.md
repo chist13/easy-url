4ea2148e8af5d6c5ef2aa03f86412f67526d3292
chist13 <chist13p@gmail.com>
2017-08-13 00:52:20 +0300

    first working commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# url
+Easy-url
+=========
+
+lightweight library for manipulation querystring
+
+## Installation
+
+    npm install easy-url
+
+## Usage
+
+    import Url form 'easy-url'
+    window.url = New Url()
+
+    url.push('search', 'foo') => http://site.com/home?search=foo
+
+    url.clear() => http://site.com/home
+
+    url.push({search: 'foo', page: 5}) => http://site.com/home?search=foo&page=5
+
+    console.log(url.obj) => {search: 'foo', page: '5'}
+
+    console.log(url.search) => '?search=foo&page=5'
---
+++ index.js
@@ -0,0 +1,142 @@
+class Events{
+    /**
+     * Create a new Events instance.
+     */
+    constructor() {
+        this._stack = []
+    }
+
+    /**
+     * push a callback to stack
+     */
+    subscribe(cb) {
+        this._stack.push(cb)
+
+        return () => this.unsubscribe(cb)
+    }
+
+    /**
+     * remove a callback from stack
+     */
+    unsubscribe(target) {
+        this._stack = this._stack.filter(e => e !== target)
+    }
+
+    /**
+     * invoke every single callback in stack
+     */
+    emit() {
+        this._stack.forEach(cb => cb())
+    }
+}
+
+class Url{
+    /**
+     * Create a new Url instance.
+     *
+     * Events class as a dependency
+     */
+    constructor() {
+        this.obj = {}
+        this.search = '?'
+        this.events = new Events()
+
+        this._init()
+    }
+
+    /**
+     * change obj and update url
+     */
+    push(key, value) {
+        if (typeof key === 'object') {
+            for (const prop in key) {
+                this._silentPush(prop, key[prop])
+            }
+        } else {
+            this._silentPush(key, value)
+        }
+
+        this._replaceUrl()
+    }
+
+    /**
+     * change property of obj withut upadeting url
+     */
+    _silentPush(key, value) {
+        value
+            ? (this.obj[key] = value)
+            : (delete this.obj[key])
+    }
+
+    /**
+     * gets full url path
+     */
+    fullPath() {
+        return location.pathname + this.search
+    }
+
+    /**
+     * add listener to url changing
+     */
+    on(callback) {
+        return this.events.subscribe(callback)
+    }
+
+    /**
+     * remove listener
+     */
+    off(callback) {
+        this.events.unsubscribe(callback)
+    }
+
+    /**
+     * apply changes to url and emit event
+     */
+    _replaceUrl() {
+        const prevSearch = this.search
+        this._updateSearch()
+
+        if (prevSearch == this.search) {
+            return
+        }
+
+        // in case you are using Turbolinks
+        const stateObj = {turbolinks: true, url: this.search}
+
+        history.replaceState(stateObj, null, this.search)
+        this.events.emit()
+    }
+
+    /**
+     * Initializes params from current location
+     */
+    _init() {
+        const url = location.search.replace(/^\?/, '')
+
+        if (!url) return
+
+        for (const e of url.split('&')) {
+            const parts = e.split('=')
+
+            this.obj[parts[0]] = parts[1]
+        }
+
+        this._updateSearch()
+    }
+
+    /**
+     * Make search string from obj property
+     */
+    _updateSearch() {
+        let array = []
+        const obj = this.obj
+
+        for (const key in obj) {
+            array.push(`${key}=${obj[key]}`)
+        }
+
+        this.search = '?' + array.join('&')
+    }
+}
+
+export default Url
